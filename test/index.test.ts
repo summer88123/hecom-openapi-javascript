8531eb04facbb6cf64052e68b24083445e92f60b
summer88123 <summer88123@163.com>
2025-04-11 00:50:52 +0800

    test: 添加对HClient的多个方法的单元测试
    新增了对`getObjects`、`getObjectDescription`、`getUserDescription`、`getConstantGroups`和`queryData`方法的单元测试，以确保这些功能的正确性和稳定性。

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,17 +1,46 @@
-import { describe, expect, test } from 'vitest';
-import HClient from '../src/index';
+import { describe, expect, test, vi } from 'vitest';
+import HClient, { QueryOptions } from '../src/index';
 
-describe('client test', () => {
-    test('base test', async () => {
-        const client = new HClient({
-            clientId: 'hlhXFvf0n3JkMMBE',
-            clientSecret: 'Qrwke0J1hzMrurGg72Dco7HLPA3iRE8p',
-            username: '18519508327',
-            apiHost: 'https://tc-dev.cloud.hecom.cn',
-        });
+describe('client', () => {
+    const client = new HClient({
+        clientId: 'hlhXFvf0n3JkMMBE',
+        clientSecret: 'Qrwke0J1hzMrurGg72Dco7HLPA3iRE8p',
+        username: '18519508327',
+        apiHost: 'https://tc-dev.cloud.hecom.cn',
+    });
+
+    test('getObjects', async () => {
         const objs = await client.getObjects();
-        console.log(objs.filter(o => o.label.length > 10).length);
+        console.log(objs.filter(o => o.label.length > 10));
         console.log(objs.length);
         expect(objs).toBeDefined();
+    });
+
+    test('getObjectDescription', async () => {
+        const mockMetaName = 'PCLargeEquipmentACSTMDetailed';
+        const description = await client.getObjectDescription(mockMetaName);
+        expect(description).toBeDefined();
+    });
+
+    test('getUserDescription', async () => {
+        const description = await client.getUserDescription();
+        expect(description).toBeDefined();
+    });
+
+    test('getConstantGroups', async () => {
+        const groups = await client.getConstantGroups();
+        expect(groups).toBeDefined();
+    });
+
+    test('queryData', async () => {
+        const queryOptions: QueryOptions = {
+            selectFields: ['id', 'name', 'entCode'],
+            pageNo: 1,
+            pageSize: 10,
+            query: { code: '1' },
+        };
+        const result = await client.queryData('PCLargeEquipmentACSTMDetailed', queryOptions);
+        console.log(result);
+        expect(result).toBeDefined();
     });
 });
