e53316317a495c14197cdb83dbe7d412bfa50065
白宇东 <summer88123@163.com>
2025-09-03 11:01:40 +0800

    feat: 更新 package.json 支持 module exports 并添加 CommonJS and ES modules 支持

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,30 @@
+import { babel } from '@rollup/plugin-babel';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import { babel } from '@rollup/plugin-babel';
 
 export default defineConfig({
     build: {
         lib: {
             entry: './src/index.ts',
             name: 'HClient',
-            fileName: 'index',
+            fileName: format => {
+                if (format === 'es') return 'index.mjs';
+                if (format === 'cjs') return 'index.cjs';
+                if (format === 'umd') return 'index.umd.cjs';
+                return 'index.js';
+            },
+            formats: ['es', 'cjs', 'umd'],
         },
         minify: true,
         rollupOptions: {
             external: ['axios', 'base-64'],
+            output: {
+                exports: 'named',
+                globals: {
+                    axios: 'axios',
+                    'base-64': 'BASE64',
+                },
+            },
             plugins: [
                 babel({
                     babelHelpers: 'bundled',
